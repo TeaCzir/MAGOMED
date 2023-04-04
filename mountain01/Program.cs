4196c163f75de7aab0dde072287f3fc2225fefc6
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-04 04:42:19 +0300

    Если Магомед не..., то ...,

---
+++ mountain01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Если Магомед не идет к горе, то гора идет к Магомеду.
+Console.Clear();
+Console.Write("Ты Магомед? ");
+string answer= Console.ReadLine();
+if(answer.ToLower() =="да")
+{
+   Console.WriteLine("Магомед не идет к горе, а х#ярит от нее, что есть мочи.");
+   Console.WriteLine("Это ОПОЛЗЕНЬ.");
+}
+if(answer.ToLower() =="нет")
+{
+   Console.WriteLine("Пи#дуй от туда.");
+   Console.WriteLine("Это ОПОЛЗЕНЬ.");
+}
